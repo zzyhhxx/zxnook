a7b8907832cc7a4d05212971537dd3b818484f7a
张兆烨 <zhangzhaoye@zhangzhaoyedeMacBook-Air.local>
2021-11-01 23:45:21 +0800

    初始化

---
+++ components/my-button/my-button.vue
@@ -0,0 +1,76 @@
+<template>
+	<view>
+		<view
+			class="btn"
+			:class="{large: isLarge, normal: isNormal, small: isSmall, usable: isUsable, unusable: isUnusable}"
+		>
+			{{text}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-button",
+		props: {
+			text: {
+				type: String,
+				default: ''
+			},
+			size: {
+				type: String,
+				default: ''
+			},
+			type: {
+				type: String,
+				default: ''
+			}
+		},
+		computed: {
+			isLarge() {
+				return this.size === 'large';
+			},
+			isNormal() {
+				return this.size === 'normal';
+			},
+			isSmall() {
+				return this.size === 'small';
+			},
+			isUsable() {
+				return this.type === 'usable';
+			},
+			isUnusable() {
+				return this.type === 'unusable';
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.btn {
+		color: #fff;
+		border-radius: 10rpx;
+		font-size: 36rpx;
+		text-align: center;
+	}
+	.large {
+		width: 604rpx;
+		height: 102rpx;
+		line-height: 102rpx;
+	}
+	.normal {
+		width: 292rpx;
+		height: 102rpx;
+		line-height: 102rpx;
+	}
+	.small {
+		width: 100rpx;
+		height: 20rpx;
+	}
+	.usable {
+		background-color: #F68685;
+	}
+	.unusable {
+		background-color: gray;
+	}
+</style>
---
+++ components/search/search.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="search">
+		<input type="text" value="" placeholder="请输入关键词" class="input"/>
+		<view class="button">
+			<image src="../../static/search.png" mode="" class="img"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"search",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+.search {
+	display: flex;
+	width: 604rpx;
+	height: 112rpx;
+	border-radius: 28rpx;
+	overflow: hidden;
+	background-color: #ffffff;
+	margin-top: 88rpx;
+}
+.button {
+	width: 112rpx;
+	height: 112rpx;
+	border-radius: 28rpx;
+	background: #F68685;
+	position: relative;
+}
+.img {
+	width: 48rpx;
+	height: 48rpx;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+.input {
+	height: 112rpx;
+	padding-left: 46rpx;
+	flex: 1;
+}
+</style>
